0799560a1ddea08e8cfeee3f2d077e395468f85d
Matvey <lmv180106@mail.ru>
2025-11-28 20:53:22 +0300

    Created java files

--- src/main/java/com/example/auction/controller/WebController.java
+++ src/main/java/com/example/auction/controller/WebController.java
@@ -1,4 +1,4 @@
-package com.example.auction;
+package com.example.auction.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
